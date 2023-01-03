42cf53452f7d57db8da317ad7b394d3883ac28d8
jpaine126 <j.paine126@gmail.com>
2023-01-03 16:42:37 -0500

    fixed install to ignore files for missing packages

--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ echo "Jeff's zsh Environment"
 source ./zsh/.zshenv
 
 # zsh
-
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME/.zshenv"
-ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc" 
-
+if ! command -v zsh >/dev/null 2>&1; then
+    echo "zsh not installed"
+else
+    ln -sf "$DOTFILES/zsh/.zshenv" "$HOME/.zshenv"
+    ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc" 
+    ln -sf "$DOTFILES/zsh/.zshrc.alias" "$HOME/.zshrc.alias"
+fi
 
 # tmux
+if ! command -v tmux >/dev/null 2>&1; then
+    echo "tmux not installed"
+else
+    ln -sf "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+fi
 
-ln -sf "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
