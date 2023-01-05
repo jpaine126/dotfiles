6881300728b075be30313530d04b5e25f1e6044d
jpaine126 <j.paine126@gmail.com>
2023-01-05 10:02:27 -0500

    added git stuff and global git ignore to installer

--- install.sh
+++ install.sh
@@ -13,3 +13,15 @@ ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 # tmux
 
 ln -sf "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+
+
+# git
+
+git config --global user.name "jpaine126"
+git config --global user.email "j.paine126@gmail.com"
+git config --global color.ui auto
+git config --global init.defaultBranch main
+
+ln -sf "$DOTFILES/.gitignore_global" "$HOME/.gitignore_global"
+git config --global core.excludes "$HOME/.gitignore_global"
+
