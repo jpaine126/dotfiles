db0df9cc802bc18489ec006e30dfe418999f5b98
jpaine126 <j.paine126@gmail.com>
2023-02-04 12:37:59 -0500

    added hoe server setup script

---
+++ scripts/install/home_server_setup.sh
@@ -0,0 +1,8 @@
+# steamlink
+
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.valvesoftware.SteamLink
+
+sudo apt-get install -y steam-devices
+sudo apt-get install -y steam
