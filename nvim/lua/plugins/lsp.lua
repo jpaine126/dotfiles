ff1436d50346a2ffdbf3ad13e7b2c4ca73640e9e
jpaine126 <j.paine126@gmail.com>
2025-08-24 15:33:55 -0400

    added nvim cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,6 +15,43 @@ return {
         end,
     },
 
+    -- completion
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-nvim-lsp'},
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- Navigate between completion items
+                    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+
+                    -- `Enter` key to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+                    -- Ctrl+Space to trigger completion menu
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                snippet = {
+                    expand = function(args)
+                      vim.snippet.expand(args.body)
+                    end,
+                }
+            })
+        end,
+        event = "InsertEnter",
+        cmd = "CmpStatus",
+    },
+
     -- lspconfig
     {
         "neovim/nvim-lspconfig",
