39eec985633322eb0cd2710e319f54cfb736f627
jpaine126 <j.paine126@gmail.com>
2025-11-14 14:49:12 -0500

    added more installs to ubuntu setup to match mac setup

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -8,3 +8,21 @@ fi
 if ! which tmux ls > /dev/null; then
     sudo apt-get install tmux
 fi
+
+if ! which nvim ls > /dev/null; then
+    sudo apt-get install nvim
+fi
+
+if ! which fzf ls > /dev/null; then
+    sudo apt-get install fzf
+    # $(brew --prefix)/opt/fzf/install --all
+fi
+
+# needed for mason
+if ! which npm ls > /dev/null; then
+    sudo apt-get install npm
+fi
+
+if ! which uv ls > /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
